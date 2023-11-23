b08509ccff2f195561b7c9027ab7e88507fb8ec2
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2023-11-23 17:19:27 +0530

    added signup page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Login() {
     return (
@@ -35,7 +36,7 @@ export default function Login() {
                             </div>
                             <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
+                                Don’t have an account yet? <Link href="/signup" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</Link>
                             </p>
                         </form>
                     </div>
