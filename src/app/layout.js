56ceaeedaa32e84ae83586f090d3b1b7d9ef380d
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2023-11-24 16:39:39 +0530

    navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
