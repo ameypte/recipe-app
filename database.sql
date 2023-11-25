52d7b6f1d35c1f3285216f2bec87bf25c5e79207
ameypte <ameypte@gmail.com>
2023-11-25 11:08:09 +0530

    updated the database

--- database.sql
+++ database.sql
@@ -89,9 +89,9 @@ CREATE TABLE IF NOT EXISTS `likes` (
 -- Dumping data for table sql12664295.likes: ~3 rows (approximately)
 DELETE FROM `likes`;
 INSERT INTO `likes` (`like_id`, `recipe_id`, `user_id`, `liked_date`) VALUES
-	(1, 1, 3, '2023-11-22 18:15:31'),
-	(2, 2, 1, '2023-11-22 18:15:31'),
-	(3, 3, 2, '2023-11-22 18:15:31');
+	(1, 1, 3, '2023-11-22 10:15:31'),
+	(2, 2, 1, '2023-11-22 10:15:31'),
+	(3, 3, 2, '2023-11-22 10:15:31');
 
 -- Dumping structure for table sql12664295.recipes
 CREATE TABLE IF NOT EXISTS `recipes` (
@@ -107,14 +107,16 @@ CREATE TABLE IF NOT EXISTS `recipes` (
   PRIMARY KEY (`recipe_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table sql12664295.recipes: ~3 rows (approximately)
+-- Dumping data for table sql12664295.recipes: ~5 rows (approximately)
 DELETE FROM `recipes`;
 INSERT INTO `recipes` (`recipe_id`, `title`, `description`, `instructions`, `prep_time`, `cook_time`, `total_time`, `servings`, `user_id`) VALUES
 	(1, 'Tomato Soup', 'A delicious soup made from tomatoes', '1. Boil tomatoes. 2. Blend them. 3. Add seasoning.', 10, 20, 30, 4, 1),
 	(2, 'Banana Smoothie', 'A refreshing smoothie made from bananas', '1. Blend bananas. 2. Add milk. 3. Blend again.', 5, 0, 5, 2, 2),
-	(3, 'Chicken Curry', 'Spicy chicken curry recipe', '1. Cook chicken. 2. Add spices. 3. Simmer.', 15, 30, 45, 6, 3);
+	(3, 'Chicken Curry', 'Spicy chicken curry recipe', '1. Cook chicken. 2. Add spices. 3. Simmer.', 15, 30, 45, 6, 3),
+	(4, 'Spaghetti Bolognese', 'Classic Italian pasta dish with rich meat sauce.', '1. Cook spaghetti according to package instructions. 2. Prepare Bolognese sauce by browning ground beef and simmering with tomatoes and herbs. 3. Serve sauce over cooked spaghetti.', 15, 30, 45, 4, 5),
+	(5, 'Chicken Alfredo Pasta', 'Creamy Alfredo sauce with grilled chicken over fettuccine.', '1. Grill chicken until fully cooked. 2. Prepare Alfredo sauce with cream, butter, and Parmesan cheese. 3. Toss cooked fettuccine with Alfredo sauce and top with grilled chicken.', 20, 25, 45, 3, 5);
 
 -- Dumping structure for table sql12664295.recipe_category
 CREATE TABLE IF NOT EXISTS `recipe_category` (
@@ -161,14 +163,33 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
--- Dumping data for table sql12664295.users: ~3 rows (approximately)
+-- Dumping data for table sql12664295.users: ~22 rows (approximately)
 DELETE FROM `users`;
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
-	(1, 'user1', 'user1@example.com', 'hashed_password_1'),
+	(1, 'user', 'user', 'user'),
 	(2, 'user2', 'user2@example.com', 'hashed_password_2'),
-	(3, 'user3', 'user3@example.com', 'hashed_password_3');
+	(3, 'user3', 'user3@example.com', 'hashed_password_3'),
+	(4, 'Bhaveshxop', 'bhaveshxop@gmail.com', '4706'),
+	(5, 'Amey', 'ameypathe@gmail.com', 'amey123'),
+	(6, 'DEN22171185', 'sonamalai12@gmail.com', 'W6n@4Kfp5qEhp5m'),
+	(7, 'asdsad', 'sdfdsf', 'sdfsdf'),
+	(8, 'asdsad', 'sdfdsf@dsfd.com', 'sdfsdf'),
+	(9, 'ffssdf', 'fesfg@gdfg.c', 'ddfsdfd'),
+	(10, 'Heramb the pro', 'fesfg@gdfg.c', 'W6n@4Kfp5qEhp5m'),
+	(11, 'keval', 'keval@gmail.com', 'keval123'),
+	(12, '', '', '123'),
+	(13, '', '', '123'),
+	(14, '', '', ''),
+	(15, '', '', ''),
+	(16, '', '', ''),
+	(17, 'fniesbf', '', ''),
+	(18, 'fniesbf', '', ''),
+	(19, 'wfbu', '', ''),
+	(20, '', '', ''),
+	(21, '', '', ''),
+	(22, '', '', '');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
