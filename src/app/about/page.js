31e7413c2c4499c932a818c0c74dfd77332d0103
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2023-12-20 00:10:31 +0530

    some changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,249 @@
-import React from 'react'
+"use client";
+import React from "react";
+import NavBar from "@/components/NavBar";
 
-export default function page() {
+export default function page({ params }) {
+  const { recipeId } = params;
   return (
-    <div>
-      this is about page
+    <>
+      <NavBar />
+      <section class="py-10 lg:py-20 bg-stone-100 font-poppins dark:bg-gray-800">
+        <div class="max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap ">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
+              <div class="lg:max-w-md">
+                <div class="px-4 pl-4 mb-6 border-l-4 border-blue-500">
+                  <span class="text-sm text-gray-600 uppercase dark:text-gray-400">
+                    Who we are?
+                  </span>
+                  <h1 class="mt-2 text-3xl font-black text-gray-700 md:text-5xl dark:text-gray-300">
+                    About Us
+                  </h1>
+                </div>
+                <p class="px-4 mb-10 text-base leading-7 text-gray-500 dark:text-gray-400">
+                  Welcome to <strong>Recipe Hub</strong>, your go-to destination
+                  for culinary inspiration and delightful recipe
+                  recommendations! At Recipe Hub, we believe that cooking is an
+                  art, and our mission is to make it easier and more enjoyable
+                  for everyone. Whether you're a seasoned chef or a kitchen
+                  novice, our platform is designed to cater to your culinary
+                  needs.
+                </p>
+                <div class="flex flex-wrap items-center">
+                  <div class="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
+                    <div class="p-6 bg-white dark:bg-gray-900">
+                      <span class="text-blue-500 dark:text-blue-400">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          class="w-10 h-10"
+                          fill="currentColor"
+                          className="bi bi-file-earmark-text"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z" />
+                          <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                        </svg>
+                      </span>
+                      <p class="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-gray-400">
+                        2097
+                      </p>
+                      <h2 class="text-sm text-gray-700 dark:text-gray-400">
+                        Recipes
+                      </h2>
+                    </div>
+                  </div>
+                  <div class="w-full px-4 mb-6 sm:w-1/2 md:w-1/2 lg:mb-6">
+                    <div class="p-6 bg-white dark:bg-gray-900">
+                      <span class="text-blue-500 dark:text-blue-400">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          class="w-10 h-10"
+                          fill="currentColor"
+                          className="bi bi-people-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
+                          />
+                          <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
+                        </svg>
+                      </span>
+                      <p class="mt-4 mb-2 text-3xl font-bold text-gray-700 dark:text-gray-400">
+                        3,590
+                      </p>
+                      <h2 class="text-sm text-gray-700 dark:text-gray-400">
+                        users
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                src="happy-chef-vector-icon.jpg"
+                alt=""
+                class="relative z-40 object-cover w-full h-full rounded"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer class="relative bg-blueGray-200 pt-8 pb-6">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap text-left lg:text-left">
+            <div class="w-full lg:w-6/12 px-4">
+              <h4 class="text-3xl fonat-semibold text-blueGray-700">
+                Let's keep in touch!
+              </h4>
+              <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
+  
+              </h5>
+              
+            </div>
+            <div class="w-full lg:w-6/12 px-4">
+              <div class="flex flex-wrap items-top mb-6">
+                <div class="w-full lg:w-4/12 px-4">
+                  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    <strong>Developed by</strong>
+                  </span>
+                  <ul class="list-unstyled">
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
+                      >
+                        Heramb Bhoodhar
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/terms?ref=njs-profile"
+                      >
+                        Amey Pathe
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/privacy?ref=njs-profile"
+                      >
+                        Bhavesh Patil
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/contact-us?ref=njs-profile"
+                      >
+                        Diksha Shingne
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/contact-us?ref=njs-profile"
+                      >
+                        Urja Wagh
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="my-6 border-blueGray-300" />
+          
+          
+<footer
+  class="flex flex-col items-center bg-neutral-200 text-center text-white dark:bg-neutral-600">
+  <div class="container pt-9">
+    <div class="mb-9 flex justify-center">
+      <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+        </svg>
+      </a>
+      <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+        </svg>
+      </a>
+      <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
+            fill-rule="evenodd"
+            clip-rule="evenodd" />
+        </svg>
+      </a>
+      <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+        </svg>
+      </a>
+      <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+        </svg>
+      </a>
+      <a href="#!" class="text-neutral-800 dark:text-neutral-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="currentColor"
+          viewBox="0 0 24 24">
+          <path
+            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+        </svg>
+      </a>
     </div>
-  )
+  </div>
+
+  <div
+    class="w-full bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+    © 2023 Copyright:
+    <a
+      class="text-neutral-800 dark:text-neutral-400"
+      href="https://tw-elements.com/"
+      >Group 1</a
+    >
+  </div>
+</footer>
+        </div>
+      </footer>
+    </>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,7 +29,7 @@ export default function NavBar() {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Recipe Hub
           </span>
         </a>
         <button
@@ -101,14 +101,6 @@ export default function NavBar() {
                 About
               </a>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
           </ul>
         </div>
         {username ? (
@@ -137,5 +129,6 @@ export default function NavBar() {
         )}
       </div>
     </nav>
+
   );
 }
