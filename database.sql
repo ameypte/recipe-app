3355b9460a09e9eb79a830eee6cdea3403287783
ameypte <ameypte@gmail.com>
2024-04-23 23:41:23 +0530

    changed database file

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 29, 2024 at 07:16 AM
+-- Generation Time: Apr 23, 2024 at 08:10 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
