85d645062e776c9470d95648d1c7bd06a5d0ab1e
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-04-24 08:33:48 +0530

    chatbot api (python backend  ) done

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from flask_cors import CORS
+import json
+
+#import custom function
+from stock_analyzer import stock_analyzer
+from chat import text_summary
+
+app = Flask(__name__) 
+CORS(app)
+
+
+@app.route('/chef', methods=['POST'])
+def summarize_text():
+    text = request.json['text']
+    isNew = request.json['isNew']
+    response = text_summary(text, isNew)
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
