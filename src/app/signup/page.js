0eef2731511be00828ec6717fed3f51b41a24ff1
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-04-25 08:55:27 +0530

    errors solved

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -40,7 +40,7 @@ export default function signup() {
     const response = await fetch("/api/auth", {
       method: "POST",
       headers: {
-        "Content-Type": "applicatiom/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username: username,
